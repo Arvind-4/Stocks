caca8b426f69c06c63c28749b086dfb184bb305d
Arvind-4 <lordarvindthegamer@gmail.com>
2023-12-02 13:28:46 +0530

    Updated: for sqlite3

--- build.sh
+++ build.sh
@@ -2,10 +2,8 @@
 
 echo "Installing Python sqlite3..."
 cd /usr/local/bin/
-yum install sqlite -y
-yum install sqlite-devel -y
-./configure --enable-optimizations --enable-loadable-sqlite-extensions
-make && make altinstall
+sudo yum install sqlite-devel -y
+sudo yum install python3-devel -y
 cd /vercel/path0/
 
 echo "Upgrade pip..."
@@ -14,19 +12,19 @@ python3.9 -m pip install --upgrade pip
 echo "Installing dependencies..."
 python3.9 -m pip install -r requirements.txt
 
-# echo "Migrating database..."
-# python3.9 manage.py makemigrations --noinput
-# python3.9 manage.py migrate --noinput
+echo "Migrating database..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
-# echo "Creating superuser..."
+echo "Creating superuser..."
 
-# DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL}
-# DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME}
-# DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD}
+DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL}
+DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME}
+DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD}
 
-# python3.9 manage.py createsuperuser \
-#     --email $DJANGO_SUPERUSER_EMAIL \
-#     --noinput || true
+python3.9 manage.py createsuperuser \
+    --email $DJANGO_SUPERUSER_EMAIL \
+    --noinput || true
 
 echo "Collecting static files..."
 python3.9 manage.py collectstatic  --noinput --clear
