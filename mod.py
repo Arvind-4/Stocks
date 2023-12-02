1942fab12c4958eda6d8efb4782db54f5033b3a9
Arvind-4 <lordarvindthegamer@gmail.com>
2023-12-02 16:06:47 +0530

    Updated: mod.py file and db

--- mod.py
+++ mod.py
@@ -3,7 +3,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 print("BASE_DIR: ", BASE_DIR)
 
-db_file = BASE_DIR / 'db.sqlite3'
+DATABASE_DIR = BASE_DIR / 'database'
+
+db_file = DATABASE_DIR / 'db.sqlite3'
 
 # # Check file permissions
 print(f'Original permissions: {oct(db_file.stat().st_mode)[-3:]}') 
--- src/settings.py
+++ src/settings.py
@@ -21,6 +21,12 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DATABASE_DIR = BASE_DIR / 'database'
+DATABASE_DIR.mkdir(
+    parents=True,
+    exist_ok=True,
+)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -90,7 +96,7 @@ WSGI_APPLICATION = 'src.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / config("DJANGO_DB_NAME", cast=str, default="db.sqlite3"),
+        'NAME': str(DATABASE_DIR / 'db.sqlite3'),
     }
 }
 
